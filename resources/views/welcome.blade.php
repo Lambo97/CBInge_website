0709a369b5e815f37a7518b725627e84f12b42ea
Lambo97 <maxime.lamborelle@gmail.com>
2019-07-04 11:22:56 +0200

    Change background of forms to dark

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,5 @@
 @extends('layouts.app')
-        <div class = "container" > 
 
-        
-     <!--   <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                
-            @endif
-
-          
-                
-                   
-                </div>
-             </div> -->  
-
-            <div class = "content">
-            
-            
-            </div>
-
-        </div>
-    </body>
-</html>
+@section('content')
+<p> Hello <p>
+@endsection
