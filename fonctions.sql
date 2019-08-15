fa33cf28b4646eed69b90a91befbda8a0fd7c9b4
Lambo97 <maxime.lamborelle@gmail.com>
2019-08-15 15:21:55 +0200

    add fonction . sql

---
+++ fonctions.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 15, 2019 at 01:21 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `cbinge`
+--
+
+--
+-- Dumping data for table `fonctions`
+--
+
+INSERT INTO `fonctions` (`id`, `nom`, `created_at`, `updated_at`) VALUES
+(1, 'Président', NULL, NULL),
+(2, 'Vice-président', NULL, NULL),
+(3, 'Trésorier', NULL, NULL),
+(4, 'Secrétaire', NULL, NULL),
+(5, 'Présidente', NULL, NULL),
+(6, 'Togé sans fonction', NULL, NULL),
+(7, 'Assistant', NULL, NULL),
+(8, 'Webmaster', NULL, NULL),
+(9, 'Maître des chants', NULL, NULL),
+(10, 'Délégué AGEL', NULL, NULL);
