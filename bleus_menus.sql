32753a0752072f13be8725e80aa7b5f194e6c586
Lambo97 <maxime.lamborelle@gmail.com>
2019-08-20 19:44:00 +0200

    fix bug

--- bleus_menus.sql
+++ bleus_menus.sql
@@ -6,7 +6,6 @@
 -- Generation Time: Aug 05, 2019 at 10:06 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -22,10 +21,10 @@ SET time_zone = "+00:00";
 
 INSERT INTO `bleus_menus` (`id`, `nom`, `lien`, `droit`, `afficher`) VALUES
 (6, 'Agenda', '/events', 0, 1),
-(3, 'Folklore', /bleus/folklore, 0, 1),
-(4, 'FAQ', /bleus/faq, 0, 1),
+(3, 'Folklore', '/bleus/folklore', 0, 1),
+(4, 'FAQ', '/bleus/faq', 0, 1),
 (5, 'Forum', NULL, 0, 1),
-(2, 'Informations', /bleus/information, 0, 1);
+(2, 'Informations', '/bleus/informations', 0, 1);
 
 
 --
