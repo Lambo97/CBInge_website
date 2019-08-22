d313b76ca2e12483d8e52bc13b71a9188be9a790
Mathieu Taquet <mathieu.taquet@hotmail.fr>
2019-08-22 21:31:26 +0200

    script photos

--- resources/views/photo/album.blade.php
+++ resources/views/photo/album.blade.php
@@ -24,11 +24,61 @@
 </div>
 
 <div class="row">
+    <?php $i = 0; ?>
     @foreach($photos as $photo)
     <div class="col-md-3 col-6 mb-2">
-        <img style="width:100%" class="mb-2" src="/photos/{{$photo->album->annee}}/{{$photo->album->id}}/{{$photo->id}}">
+        <img style="width:100%;" class="mb-2" src="/photos/{{$photo->album->annee}}/{{$photo->album->id}}/{{$photo->id}}" onclick="openModal(); currentSlide(<?php echo $i; ?>);">
     </div>
+    <?php $i ++ ?>
     @endforeach
 </div>
+
+<div id="myModal" class="modal">
+    <span class="close cursor" onclick="closeModal();">&times;</span>
+    <div class="modal-content">
+        @foreach($photos as $photo)
+        <div class="mySlides" style="width: 100%; height: 90vh;">
+            <img src="/photos/{{$photo->album->annee}}/{{$photo->album->id}}/{{$photo->id}}" style="width: 100%; height: 100%; object-fit: contain;">
+        </div>
+        @endforeach
+
+        <a class="prev" onclick="plusSlides(-1)"><</a>
+        <a class="next" onclick="plusSlides(1)">></a>
+    </div>
+</div>
+
+
+<script>
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 0}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+</script>
+
 
 @endsection
