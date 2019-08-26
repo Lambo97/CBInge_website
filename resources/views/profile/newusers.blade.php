1d2c5fdefafe97feafff85661cf280f3f714cceb
louissdeniss <52701182+louissdeniss@users.noreply.github.com>
2019-08-26 21:56:51 +0200

    Update newusers.blade.php

--- resources/views/profile/newusers.blade.php
+++ resources/views/profile/newusers.blade.php
@@ -5,41 +5,33 @@
 @endsection
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card bg-dark">
-            <div class="card-header">Bleus à approuver</div>
-
-            <div class="card-body">
-
-                <table class="table">
-                    <tr>
-                        <th>Nom</th>
-                        <th>Prenom</th>
-                        <th>Surnom</th>
-                        <th>Inscris le</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    @forelse ($users as $user)
-                        <tr>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->prenom }}</td>
-                            <td>{{ $user->surnom }}</td>
-                            <td>{{ date("d-m-Y H:i:s", strtotime($user->created_at)) }}</td>
-                            <td><a href="{{ '/profile/approve/'.$user->id }}"
-                                   class="btn btn-primary btn-sm">Approver</a></td>
-                            <td><a href="{{ '/profile/destroy/'.$user->id }}"
-                                    class="btn btn-primary btn-sm">Supprimer</a></td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4">Pas d'utilisateur à approuver</td>
-                        </tr>
-                    @endforelse
-                </table>
-            </div>
-        </div>
-    </div>
+<h1 class="mb-3">Bleus à approuver</h1>
+<div class="row">
+    <table class="table approve">
+        <tr class="approve_title">
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Surnom</th>
+            <th>Inscris le</th>
+            <th></th>
+            <th></th>
+        </tr>
+        @forelse ($users as $user)
+            <tr class="approve_user">
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->prenom }}</td>
+                <td>{{ $user->surnom }}</td>
+                <td>{{ date("d-m-Y H:i:s", strtotime($user->created_at)) }}</td>
+                <td><a href="{{ '/profile/approve/'.$user->id }}"
+                        class="btn buttons-green btn-primary btn-sm">Approuver</a></td>
+                <td><a href="{{ '/profile/destroy/'.$user->id }}"
+                        class="btn buttons-green btn-primary btn-sm">Supprimer</a></td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4">Pas d'utilisateur à approuver</td>
+            </tr>
+        @endforelse
+    </table>
 </div>
 @endsection
